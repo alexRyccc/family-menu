7d04e16f1cd02df8f458972e35d53b13bced2fca
alexRyccc <45168848@qq.com>
2026-08-14 00:03:23 +0800

    Avoid launcher log file contention

--- launch-family-menu-server.bat
+++ launch-family-menu-server.bat
@@ -1,4 +1,4 @@
 @echo off
 set PORT=3001
 cd /d C:\opencode\family-menu
-C:\taskA\nodejs\node.exe server.js >> C:\opencode\family-menu\family-menu-3001.log 2>> C:\opencode\family-menu\family-menu-3001.err.log
+C:\taskA\nodejs\node.exe server.js
